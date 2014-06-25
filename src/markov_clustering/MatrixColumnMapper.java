1a5db469f7071de4280ad3775e5b9fba4ddce7a7
Michele Carignani <michele.carignani@gmail.com>
2014-06-25 15:55:40 +0200

    docs

--- src/markov_clustering/MatrixColumnMapper.java
+++ src/markov_clustering/MatrixColumnMapper.java
@@ -7,13 +7,19 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class MatrixColumnMapper extends Mapper<LongWritable, Text, Text, Text> {
+	
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    	
         String[] line = value.toString().split("\t");
         String[] indicesAndValue = line[0].split(",");
+        
         Text outputKey = new Text();
         Text outputValue = new Text();
+        
        	outputKey.set(indicesAndValue[1]);
         outputValue.set("B," + indicesAndValue[0] + "," + line[1]);
+        
         context.write(outputKey, outputValue);
     }
+    
 }
