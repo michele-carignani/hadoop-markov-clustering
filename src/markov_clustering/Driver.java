39e60d67a45e1057c3090d19f7e2cb3204d87f2b
Michele Carignani <michele.carignani@gmail.com>
2014-06-25 15:55:16 +0200

    more documentation and spacing

--- src/markov_clustering/Driver.java
+++ src/markov_clustering/Driver.java
@@ -21,48 +21,81 @@ public class Driver {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
+		
 		Configuration clusterConf = new Configuration();
+		
 		int iterations = 0;
 		int maxIterations = Integer.parseInt(args[2].trim());
+		int current, next, inflate = 2;
+		
 		Path[] working = new Path[2];
+		
 		boolean converged = false;
+		
+		
 		/** Directory for the original matrix M0 */
 		Path inputfolder = new Path(args[0]);
 		clusterConf.setDouble("threshold", 0.00001);
 		clusterConf.setBoolean("converged", true);
+		
+		
 		/** Working directory 1, at first step initialize with copy of original matrix M0 */
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm");
 	    Calendar cal = Calendar.getInstance();
 		working[0] = new Path("/tmp/intmul"+dateFormat.format(cal.getTime()));
+		
+		
 		/** Working directory 2, at first step empty */
 		working[1] = new Path("/tmp/intmul2"+dateFormat.format(cal.getTime()));
+		
+		working[inflate] = new Path("/tmp/inflate"+dateFormat.format(cal.getTime()));
+		
 		FileSystem fs = FileSystem.get(clusterConf);
+		
+		
 		try {
 			/** Copy original matrix to the first input folder*/
 			DistCopy.copy(inputfolder, working[0]);
-		int current, next;
-		do {
-			current = iterations%2;
-			next = (iterations+1)%2;
-			/** Ensure the directory doesn't exist*/
-			fs.delete(working[next], true);
-			MatrixMultiplication.run(clusterConf, inputfolder, working[current], working[next]);
-			converged = Convergence.run(clusterConf, working[current], working[next]);
-			iterations++;
-		} while (!converged && maxIterations > iterations);
-		if (converged) {
-			System.out.println("Reached convergency in "+iterations+" iterations");
-			System.out.println(clusterConf.getDouble("convergedDouble", 0.0));
-		} else 
-			System.out.println("Convergency not reached");
+			
+			do {
+				current = iterations%2;
+				next = (iterations+1)%2;
+				
+				/** Ensure the directory doesn't exist*/
+				fs.delete(working[next], true);
+				
+				/** Run a two step matrix multiplication map-reduce job */
+				MatrixMultiplication.run(clusterConf, inputfolder, working[current], working[inflate]);
+				
+				Inflation.run(working[inflate], working[next]);
+				
+				/** Run a one step map-reduce job to check convergece */
+				converged = Convergence.run(clusterConf, working[current], working[next]);
+				
+				iterations++;
+				
+			} while (!converged && maxIterations > iterations);
+			if (converged) {
+				
+				System.out.println("Reached convergency in "+iterations+" iterations");
+				System.out.println(clusterConf.getDouble("convergedDouble", 0.0));
+				
+			} else { 
+				
+				System.out.println("Convergency not reached");
+				
+			}
 		} catch(IOException io) {
+			
 			io.printStackTrace();
 			System.exit(-1);
 			
 		} catch(InterruptedException ie) {
+			
 			System.out.println(ie.getMessage());
 			ie.printStackTrace();
 			System.exit(-1);
+			
 		} catch (ClassNotFoundException cnf) {
 			cnf.printStackTrace();
 			System.exit(-1);
